0aebf6e911c819f91216bba8ebd717288aed81b1
skaterdav21 <skaterdav21@gmail.com>
2026-03-23 11:33:17 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import './globals.css';
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html>
+    <html lang="en">
       <body>{children}</body>
     </html>
   );
